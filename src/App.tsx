9567ea933221c63db2ac36ff153a14faa4433453
Sooyoung <sooyoung159@naver.com>
2022-11-13 09:06:12 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const w = document.createElement("canvas").getContext("webgl");
+  const d = w?.getExtension("WEBGL_debug_renderer_info");
+  const g = (d && w?.getParameter(d.UNMASKED_RENDERER_WEBGL)) || "";
+  return <div className="App">{g}</div>;
 }
 
 export default App;
